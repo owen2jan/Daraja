e7e01e52a28be90f697cdf2775aa46c19e31923b
jan <owen2jan@gmail.com>
2022-09-01 13:39:44 +0300

    minified

--- functions.php
+++ functions.php
@@ -1,22 +1,21 @@
 <?php
-function token($key)
+function token()
 {
     $curl = curl_init('https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials');
+    curl_setopt($curl, CURLOPT_HTTPHEADER, ['Authorization:Basic ' . base64_encode('FhRAJvX9AkjbVCJkDLNMUdKoZ3zKVVAX:3xjsp2gRl9G642qe')]);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, ['Authorization:Basic ' . base64_encode($key)]);
-    return json_decode(curl_exec($curl))->access_token;
+    $token = json_decode(curl_exec($curl))->access_token;
+    curl_close($curl);
+    return $token;
 }
-function darajaRequest($key, $url, $data)
+function darajaRequest($url, $data)
 {
     $curl = curl_init($url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, ['Content-Type: Application/json', 'Authorization:Bearer ' . token()]);
     curl_setopt($curl, CURLOPT_POST, true);
     curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
-    curl_setopt($curl, CURLOPT_HTTPHEADER, [
-        'Content-Type:Application/json; Charset=utf8',
-        'Authorization:Bearer ' . token($key),
-    ]);
-    return json_decode(curl_exec($curl), true);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($curl);
+    curl_close($curl);
+    return $response;
 }
--- index.php
+++ index.php
@@ -1,18 +1,19 @@
 <?php
-    require_once 'functions.php';
-    $key = 'key:secret';
-    //registeUrls
-    var_dump(darajaRequest($key, 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl',[
-        "ShortCode" => 00000,
-        "ResponseType" => "Completed",
-        "ConfirmationURL" => "",
-        "ValidationURL" => ""
+    include_once 'functions.php';
+
+    //registerUrls
+    var_dump(darajaRequest('https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl', [
+        "ShortCode" => 600247,
+        "ResponseType" => "Cancelled",
+        "ConfirmationURL" => "https://5ade-197-237-240-21.ngrok.io/confirmation",
+        "ValidationURL" => "https://5ade-197-237-240-21.ngrok.io/validation"
     ]));
+
     //simulate
-    var_dump(darajaRequest($key, 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/simulate',[
-        "CommandID" => "CustomerBuyGoodsOnline",
-        "Amount" => 1000,
-        "Msisdn" => "254724628580",
-        "BillRefNumber" => "00000",
-        "ShortCode" => 00000
-    ]));
+    // var_dump(darajaRequest('https://sandbox.safaricom.co.ke/mpesa/c2b/v1/simulate', [
+    //     "CommandID" => "CustomerBuyGoodsOnline",
+    //     "Amount" => 1000,
+    //     "Msisdn" => "254724628580",
+    //     "BillRefNumber" => "00000",
+    //     "ShortCode" => 00000
+    // ]));
